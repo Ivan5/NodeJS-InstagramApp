c614f32297906b43b6f2c41a3c3a754f4f8d0d0e
Ivan5 <converse_ivan@hotmail.com>
2018-09-30 18:01:16 -0500

    Add instagram login and get profile data

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const engine = require('ejs-mate');
 const path = require('path');
+const session = require('cookie-session');
 const app = express();
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 //middlewares
 app.use(morgan('dev'));
-
+app.use(session({
+  secret: 'mysecretword',
+  signed: true
+}));
 //routes
 app.use(require('./routes/index'));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,21 +11,49 @@ router.get('/',(req,res)=>{
   res.render('index');
 });
 
+const redirectURL = 'http://localhost:3000/handleauth';
 router.get('/auth/instagram',(req,res) => {
-
+  res.redirect(
+    instagram.getAuthorizationUrl(redirectURL, {
+      scope: ['basic', 'likes'],
+      state: 'your state'
+    })
+  );
 });
 
+router.get('/handleauth', async (req,res) => {
+  try {
+    const code = req.query.code;
+    const data = await instagram.authorizeUser(code, redirectURL);
+    //console.log(data);
+    req.session.access_token = data.access_token;
+    req.session.user_id = data.user.id;
 
+    instagram.config.accessToken = req.session.access_token;
+   res.redirect('/profile', {user: profileData.data, posts: media.data});
+    //console.log(instagram);
+  } catch (error) {
+    res.json(error);
+  }
+  
+})
 router.get('/login',(req,res) => {
-
+  res.redirect('/auth/instagram');
 });
 
 router.get('/logout',(req,res) => {
 
 });
 
-router.get('profile', (req,res) => {
-
+router.get('/profile', async (req,res) => {
+  try {
+    const profileData = await instagram.get('users/self');
+    const media = await instagram.get('users/self/media/recent');
+    console.log(profileData);
+    res.render('profile');
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
